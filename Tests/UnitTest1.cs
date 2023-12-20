5060c1d7ecfa42faf74d55034408c843b4eacd6c
miszalbot <miszalbot@gmail.com>
2023-12-20 19:00:50 +0100

    Test push

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tests;
+
+public class FunctionTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void PrvniTest()
+    {
+        Assert.Pass();
+    }
+}
